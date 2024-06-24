55a5d090f539bfeae2b06f17cf7556ee1cc8b4bd
rodineyw <rodineyw@yahoo.com.br>
2024-06-23 22:48:35 -0300

    feat: setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='video2livephoto',
+    version='0.1.0',
+    author='Ródiney Wanderson',
+    author_email='rodineyw@yahoo.com.br',
+    description='Um pacote para converter vídeos em live-fotos, comum em dispositivos iOS.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/seu_usuario/video2livephoto',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'Pillow'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.7',
+)
