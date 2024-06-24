ab6abc12b53d84e679588fd7208e92d279732d97
rodineyw <rodineyw@yahoo.com.br>
2024-06-23 23:10:12 -0300

    feat: setup para subir para o pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    page_description = f.read()
+
 setup(
     name='video2livephoto',
     version='0.1.0',
     author='Ródiney Wanderson',
     author_email='rodineyw@yahoo.com.br',
     description='Um pacote para converter vídeos em live-fotos, comum em dispositivos iOS.',
-    long_description=open('README.md').read(),
+    long_description=page_description,
     long_description_content_type='text/markdown',
     url='https://github.com/seu_usuario/video2livephoto',
     packages=find_packages(),
